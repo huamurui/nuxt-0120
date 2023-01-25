1fc6d9d5ac6b1bf9fee85821ace0bc52218f8381
huamu rui <yunTai013@outlook.com>
2023-01-25 10:12:19 +0000

    init1

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <h2>aaa</h2>
-    <p>{{  }}</p>
+    <p>{{ data }}</p>
+    <!-- <p>{{ response }}</p> -->
   </div>
 </template>
 
 <script setup lang="ts">
-/*
+
+const { data, pending, error, refresh } = await useFetch('https://api.nuxtjs.dev/mountains',{
+  // 不写就是全拿...
+})
+
 interface Meow {
   title: string
   article: string
 }
 
-const { find } = useStrapi()
-const response = await find<Meow>('meows')
-*/
+// const { find } = useStrapi()
+// const response = await find<Meow>('meows')
+
 </script>
--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,27 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/strapi'],
-  strapi: {
-    // Options
-    url: process.env.STRAPI_URL || 'http://localhost:1337',
-    prefix: '/api',
-    version: 'v4',
-    cookie: {},
-    cookieName: 'strapi_jwt'  
-  }
-  // runtimeConfig: {
-  //   strapi: { // nuxt/strapi options available server-side
-  //     url: 'http://example-strapi-instance:1337'
-  //   },
-  //   public: {
-  //     strapi: { // nuxt/strapi options available client-side
-  //       url: 'https://strapi.example.com'
-  //     }
+
+  // https://github.com/nuxt/nuxt/issues/12748
+  // https://github.com/vitejs/vite/issues/1925
+  // vite: {
+  //   server: {
+  //     hmr: {
+  //       protocol: 'wss',
+  //       port: 22300,
+  //       clientPort: 443,
+  //       path: 'hmr/',
+  //       timeout: 3,
+  //     },
   //   }
   // },
-  // // nuxt/strapi options available on both client and server
-  // strapi: {
-  //   prefix: '/api'
-  // }
-  // // ...
+  strapi: {
+    // // Options
+    // url: process.env.STRAPI_URL || 'http://localhost:1337',
+    // prefix: '/api',
+    // version: 'v4',
+    // cookie: {},
+    // cookieName: 'strapi_jwt'  
+  }
 })
